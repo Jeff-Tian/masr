a5eabfe0f0ba3aa9d6917f42a3605b5544755ce9
Jeff Tian <jefftian@hcdlearning.com>
2017-12-28 14:53:19 +0800

    Use json string

--- src/main/java/masr/idempotency/AudioToText.java
+++ src/main/java/masr/idempotency/AudioToText.java
@@ -1,5 +1,6 @@
 package masr.idempotency;
 
+import com.alibaba.fastjson.JSON;
 import com.iflytek.msp.cpdb.lfasr.model.Message;
 import masr.common.FileHelper;
 import redis.clients.jedis.Jedis;
@@ -17,7 +18,7 @@ public class AudioToText {
         if (cache == null) {
             Message message = Lfasr.Lfasr.convertAudioToText(filePath);
 
-            cache = String.valueOf(message);
+            cache = JSON.toJSONString(message);
             jedis.set(hash, cache);
         }
 
